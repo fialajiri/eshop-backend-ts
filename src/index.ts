a432a57f02253c12dbc173edd22ad9873644008f
Jiri Fiala <jirik.fiala@post.cz>
2022-01-26 15:37:19 +0100

    heroku deploy 4

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,23 @@ const PORT : string|number = process.env.PORT || 5000;
 const mongoDbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oahmw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const start = async () => {
-  if (
-    !process.env.DB_USER ||
-    !process.env.DB_PASSWORD ||
-    !process.env.DB_NAME
-  ) {
-    throw new Error("DB_USER, DB_PASSWORD and DB_NAME must be defined");
-  }
-  if (
-    !process.env.JWT_SECRET ||
-    !process.env.JWT_EXPIRY ||
-    !process.env.REFRESH_TOKEN_SECRET ||
-    !process.env.REFRESH_TOKEN_EXPIRY
-  ) {
-    throw new Error(
-      "JWT_SECRET, JWT_EXPIRY, REFRESH_TOKEN_SECRET and REFRESH_TOKEN_EXPIRY must be defined"
-    );
-  }
+  // if (
+  //   !process.env.DB_USER ||
+  //   !process.env.DB_PASSWORD ||
+  //   !process.env.DB_NAME
+  // ) {
+  //   throw new Error("DB_USER, DB_PASSWORD and DB_NAME must be defined");
+  // }
+  // if (
+  //   !process.env.JWT_SECRET ||
+  //   !process.env.JWT_EXPIRY ||
+  //   !process.env.REFRESH_TOKEN_SECRET ||
+  //   !process.env.REFRESH_TOKEN_EXPIRY
+  // ) {
+  //   throw new Error(
+  //     "JWT_SECRET, JWT_EXPIRY, REFRESH_TOKEN_SECRET and REFRESH_TOKEN_EXPIRY must be defined"
+  //   );
+  // }
 
   try {
     await mongoose.connect(mongoDbUrl);
